82104a881f9d541fc6b877694fbeb0b9db0acc16
James Tobey <jctobey@eckerd.edu>
2018-11-27 18:44:46 -0600

    so close I can smell itgit add .!

--- public/assets/js/GPreq/getLostFoundPets.js
+++ public/assets/js/GPreq/getLostFoundPets.js
@@ -73,6 +73,7 @@ $(document).ready(function () {
     rowClick: function (e, row) {
       var rowClicked = row.getData();
       console.log(rowClicked.id)
+      map.removeLayer(markers);
      }
    
   });
