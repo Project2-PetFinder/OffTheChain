e0ca398fe592c33972dbc4e234e5643aade998f2
James Tobey <jctobey@eckerd.edu>
2018-11-28 09:21:19 -0600

    added alter:true to sequelize sync code in hopes that sequelize will add new column to our DB...throwing error right now

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(router)
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
+db.sequelize.sync({alter:true}).then(function() {
     app.listen(PORT, function() {
       console.log("App listening on PORT " + PORT);
     });
