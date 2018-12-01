c6336db7746f3224656738898b18301341e0f0ca
James Tobey <jctobey@eckerd.edu>
2018-12-01 09:06:50 -0600

    changed tabulator stuff

--- public/assets/js/GPreq/getLostFoundPets.js
+++ public/assets/js/GPreq/getLostFoundPets.js
@@ -99,15 +99,15 @@ $(document).ready(function () {
     placeholder: "No Data Set",
     columns: [
       { title: "Name", field: "name", sorter: "string", headerFilter: "input" },
+      { title: "Type", field: "type", align: "center", formatter: "string", headerFilter: "input" },
       { title: "Address Found", field: "Address", sorter: "string", width: 175 },
       { title: "At AAC?", field: "at_AAC", sorter: "string", headerFilter: "input" },
       { title: "Date Lost/Found", field: "date", sorter: "date", align: "center" },
       { title: "Looks Like", field: "looks_like", align: "center", formatter: "string", headerFilter: "input" },
-      { title: "Type", field: "type", align: "center", formatter: "string", headerFilter: "input" },
       { title: "Color", field: "color", align: "center", formatter: "string", headerFilter: "input" },
       { title: "Sex", field: "sex", align: "center", formatter: "string", headerFilter: "input" },
       { title: "Age", field: "age", align: "center", formatter: "string", headerFilter: "input" },
-      { title: "Picture",field: "image_link", formatter:"image",align:"center",maxWidth:25,maxHeight :25},
+      { title: "Picture",field: "image_link", formatter:"image",align:"center",height:50,width :50},
     ],
     
     
